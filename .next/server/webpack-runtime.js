c10af77aa67c31d4335787b1b7ead1045699455b
Dhaval Fatnani <dhavalfatnani017@gmail.com>
2026-03-05 03:25:01 +0530

    Update app paths manifest to include new dashboard page, refresh webpack runtime hash for improved performance, and enhance client reference management for better navigation across dashboard pages.

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -30,7 +30,7 @@
 /******/ 		// Execute the module function
 /******/ 		var threw = true;
 /******/ 		try {
-/******/ 			__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 			__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
 /******/ 			threw = false;
 /******/ 		} finally {
 /******/ 			if(threw) delete __webpack_module_cache__[moduleId];
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("893b4b402d8b008c")
+/******/ 		__webpack_require__.h = () => ("83c7c50a2003ad90")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
